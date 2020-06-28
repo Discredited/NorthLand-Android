6af8a1115629cb46591ce9eb9adb01e720f12a87
June <junemvp@foxmail.com>
2020-06-28 18:11:24 +0800

    CharacterPotential

---
+++ app/src/main/java/com/june/northland/feature/character/CharacterPotentialFragment.kt
@@ -0,0 +1,14 @@
+package com.june.northland.feature.character
+
+import com.june.northland.R
+import com.june.northland.base.component.BaseDialogFragment
+
+class CharacterPotentialFragment : BaseDialogFragment() {
+
+    private var mPotential = 1000
+
+    override fun getLayoutResId(): Int = R.layout.fragment_character_potential
+
+    override fun initView() {
+    }
+}
