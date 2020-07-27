2370b0ddb2f552a13490a2eb3ed1f29828745d89
June <junemvp@foxmail.com>
2020-07-27 18:05:36 +0800

    role detail

---
+++ app/src/main/java/com/june/northland/feature/role/RoleDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.june.northland.feature.role
+
+import com.june.northland.R
+import com.june.northland.base.component.BaseActivity
+
+class RoleDetailActivity : BaseActivity() {
+
+    override fun getLayoutResId(): Int = R.layout.activity_role_detail
+
+    override fun initView() {
+    }
+
+    override fun loadData() {
+    }
+}
