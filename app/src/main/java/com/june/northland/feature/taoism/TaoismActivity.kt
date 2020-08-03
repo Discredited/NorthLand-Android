e0683271251198579f0b9081cf099dd0b0985417
June <junemvp@foxmail.com>
2020-08-03 18:01:05 +0800

    Taoism

---
+++ app/src/main/java/com/june/northland/feature/taoism/TaoismActivity.kt
@@ -0,0 +1,13 @@
+package com.june.northland.feature.taoism
+
+import com.june.northland.base.component.BaseActivity
+
+class TaoismActivity : BaseActivity() {
+    override fun getLayoutResId(): Int = 0
+
+    override fun initView() {
+    }
+
+    override fun loadData() {
+    }
+}
