6ad2883e2e99e88e2253e466159a8b61ae20beb6
June <junemvp@foxmail.com>
2020-08-04 19:21:03 +0800

    Taoism

--- app/src/main/java/com/june/northland/feature/taoism/TaoismActivity.kt
+++ app/src/main/java/com/june/northland/feature/taoism/TaoismActivity.kt
@@ -1,9 +1,11 @@
 package com.june.northland.feature.taoism
 
+import com.june.northland.R
 import com.june.northland.base.component.BaseActivity
 
 class TaoismActivity : BaseActivity() {
-    override fun getLayoutResId(): Int = 0
+    
+    override fun getLayoutResId(): Int = R.layout.activity_taoism
 
     override fun initView() {
     }
