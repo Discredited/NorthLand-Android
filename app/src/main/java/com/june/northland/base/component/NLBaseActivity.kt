df344391fdf884c412d1a0d0e1d4387d44ca91e7
June <xujun2@tinman.cn>
2021-05-14 22:10:16 +0800

    尝试封装Base中的公共代码

--- app/src/main/java/com/june/northland/base/component/NLBaseActivity.kt
+++ app/src/main/java/com/june/northland/base/component/NLBaseActivity.kt
@@ -2,29 +2,19 @@ package com.june.northland.base.component
 
 import androidx.viewbinding.ViewBinding
 import com.june.base.basic.part.BaseActivity
-import com.june.northland.R
 
 abstract class NLBaseActivity<V : ViewBinding> : BaseActivity<V>() {
 
-    private var mLoadingView: LoadingView? = null
+    private val mDelegate: NLDelegate? by lazy { NLDelegate(this) }
 
-    fun showLoading(loadingText: String? = null) {
-        if (null == mLoadingView) {
-            mLoadingView = mBinding.root.findViewById(R.id.v_loading)
-            if (null == mLoadingView) {
-                mLoadingView = LoadingView(this)
-                mLoadingView!!.id = R.id.v_loading
-            }
-        }
-        mLoadingView?.apply {
-            setLoadingText(loadingText)
-            if (!isShowing()) {
-                show()
-            }
-        }
+    fun showLoading(
+        isAllowDismiss: Boolean = false,
+        loadingText: String? = null
+    ) {
+        mDelegate?.showLoading(isAllowDismiss, loadingText)
     }
 
     fun hideLoading() {
-
+        mDelegate?.hideLoading()
     }
 }
