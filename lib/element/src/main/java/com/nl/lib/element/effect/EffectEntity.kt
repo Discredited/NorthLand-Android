3d2155e21aad35d4588a06d461b1e2897321f5cb
June <junemvp@foxmail.com>
2022-04-09 09:57:28 +0800

    feat:效果枚举定义

--- lib/element/src/main/java/com/nl/lib/element/effect/EffectEntity.kt
+++ lib/element/src/main/java/com/nl/lib/element/effect/EffectEntity.kt
@@ -2,6 +2,7 @@ package com.nl.lib.element.effect
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.nl.lib.element.enum.PropertyEnum
 
 /**
  * 技能、装备、道具等等效果
@@ -9,24 +10,24 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "effects")
 data class EffectEntity(
     @PrimaryKey val id: String,
-    val relationType: Int,          // 关联类型：技能、装备、秘籍、道心
-    val relationId: String,         // 关联id：技能ID  装备ID  秘籍ID  道心ID
-    val relationName: String,       // 关联名称
-    val desc: String,               // 效果描述
-    val activeLevel: Int = 0,       // 激活等级
-    val isActive: Boolean = false,  // 是否激活
-    val effectAction: Int = EffectEnum.EFFECT_ACTION_BUFF,      // 效果作用：0-buff,1-普通伤害,2-真实伤害
-    val priority: Int = EffectEnum.EFFECT_PRIORITY_MIDDLE,          // 效果优先级：0-high,5-middle,10-low
-    val effectFrom: Int = 0,        // 效果来源：攻防血速爆抗命闪护盾
-    val effectTarget: Int = 0,      // 效果目标：攻防血速爆抗命闪护盾
-    val effect: Int = 0,            // 效果：1-增益、-1-减益 0-无效
-    val effectObject: Int,          // 效果作用对象
-    val effectScope: Int = EffectEnum.EFFECT_SCOPE_SINGLE,           // 效果作用范围
-    val effectType: Int = EffectEnum.EFFECT_TYPE_PERCENTAGE,            // 效果类型：0-百分比 / 1-固定值
-    val percentage: Float = 0F,          // 百分比
-    val percentageGrowth: Float = 0F,    // 百分比成长
-    val value: Int = 0,                 // 固定值
-    val valueGrowth: Int = 0,           // 固定值成长
-    val triggerProbability: Float = 1F,         // 效果触发概率
-    val triggerProbabilityGrowth: Float = 0F    // 效果触发概率成长
+    val relationType: Int,              // 关联类型：技能、装备、秘籍、道心
+    val relationId: String,             // 关联id：技能ID  装备ID  秘籍ID  道心ID
+    val relationName: String,           // 关联名称
+    val desc: String,                   // 效果描述
+    val activeLevel: Int = 0,           // 激活等级
+    val isActive: Boolean = false,      // 是否激活
+    val priority: Int = EffectEnum.EFFECT_PRIORITY_MIDDLE,      // 效果优先级：0-high,5-middle,10-low
+    val effectFrom: Int = PropertyEnum.PRO_NULL,                // 效果来源：攻防血速爆抗命闪护盾
+    val effectTarget: Int = PropertyEnum.PRO_NULL,              // 效果目标：攻防血速爆抗命闪护盾
+    val effectMode: Int = EffectEnum.EFFECT_MODE_BUFF,          // 效果模式：0-buff,1-普通伤害,2-真实伤害
+    val effectAction: Int = EffectEnum.EFFECT_ACTION_NULL,      // 效果作用：1-增益、-1-减益 0-无效
+    val effectObject: Int = EffectEnum.EFFECT_OBJECT_OTHERS,    // 效果作用对象
+    val effectScope: Int = EffectEnum.EFFECT_SCOPE_SINGLE,      // 效果作用范围
+    val effectType: Int = EffectEnum.EFFECT_TYPE_PERCENTAGE,    // 效果类型：0-百分比 / 1-固定值
+    val percentage: Float = 0F,                  // 百分比
+    val percentageGrowth: Float = 0F,            // 百分比成长
+    val value: Int = 0,                          // 固定值
+    val valueGrowth: Int = 0,                    // 固定值成长
+    val triggerProbability: Float = 1F,          // 效果触发概率
+    val triggerProbabilityGrowth: Float = 0F     // 效果触发概率成长
 )
--- lib/element/src/main/java/com/nl/lib/element/effect/EffectEnum.kt
+++ lib/element/src/main/java/com/nl/lib/element/effect/EffectEnum.kt
@@ -2,24 +2,35 @@ package com.nl.lib.element.effect
 
 object EffectEnum {
 
-    const val EFFECT_ACTION_BUFF = 0         // BUFF效果
-    const val EFFECT_ACTION_DAMAGE = 1       // 普通伤害效果
-    const val EFFECT_ACTION_DAMAGE_TRUE = 2  // 真实伤害效果
+    const val EFFECT_MODE_BUFF = 0              // BUFF效果
+    const val EFFECT_MODE_DAMAGE = 0            // BUFF效果
+    const val EFFECT_MODE_DAMAGE_TRUE = 0       // BUFF效果
+
+    ////////// 效果作用
+    const val EFFECT_ACTION_BUFF = 1            // 增益
+    const val EFFECT_ACTION_NULL = 0            // 无效
+    const val EFFECT_ACTION_DEBUFF = -1         // 减益
+
+
+    ////////// 作用对象
+    const val EFFECT_OBJECT_SELF = 1            // 增益
+    const val EFFECT_OBJECT_OTHERS = 2          // 无效
 
     ////////// 作用范围
-    const val EFFECT_SCOPE_SELF = 0      // 自身
-    const val EFFECT_SCOPE_SINGLE = 1      // 单体
-    const val EFFECT_SCOPE_ALL = 2      // 全体
-    const val EFFECT_SCOPE_ROW = 3      // 行
-    const val EFFECT_SCOPE_COLUMN = 4      // 列
-    const val EFFECT_SCOPE_AROUND = 5      // 周围（十字范围）
-    const val EFFECT_SCOPE_ATTACK_HIGH = 6     // 攻击最高
-    const val EFFECT_SCOPE_HEALTH_LOW = 7      // 生命最低
+    const val EFFECT_SCOPE_SINGLE = 1           // 单体
+    const val EFFECT_SCOPE_ALL = 2              // 全体
+    const val EFFECT_SCOPE_ROW = 3              // 行
+    const val EFFECT_SCOPE_COLUMN = 4           // 列
+    const val EFFECT_SCOPE_AROUND = 5           // 周围（十字范围）
+    const val EFFECT_SCOPE_ATTACK_HIGH = 6      // 攻击最高
+    const val EFFECT_SCOPE_HEALTH_LOW = 7       // 生命最低
 
+    ////////// 效果优先级
     const val EFFECT_PRIORITY_LOW = 1           // 优先级低
-    const val EFFECT_PRIORITY_MIDDLE = 5      // 优先级中等
-    const val EFFECT_PRIORITY_HIGH = 10      // 优先级高
+    const val EFFECT_PRIORITY_MIDDLE = 5        // 优先级中等
+    const val EFFECT_PRIORITY_HIGH = 10         // 优先级高
 
-    const val EFFECT_TYPE_PERCENTAGE = 0     // 百分比类型
-    const val EFFECT_TYPE_VALUE = 1          // 固定值类型
+    ////////// 效果类型
+    const val EFFECT_TYPE_PERCENTAGE = 0        // 百分比类型
+    const val EFFECT_TYPE_VALUE = 1             // 固定值类型
 }
