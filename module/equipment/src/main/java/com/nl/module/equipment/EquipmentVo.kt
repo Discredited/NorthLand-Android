ce7e2084931fda64c5245881e21a62ae0c74868a
June <junemvp@foxmail.com>
2022-07-10 01:10:29 +0800

    feat:序列化错误的问题

--- module/equipment/src/main/java/com/nl/module/equipment/EquipmentVo.kt
+++ module/equipment/src/main/java/com/nl/module/equipment/EquipmentVo.kt
@@ -6,30 +6,30 @@ import com.nl.component.common.PropertyHelper
 import com.nl.module.equipment.strengthen.StrengthAdditionVo
 
 data class EquipmentVo(
+    val id: String = "111111",
     val name: String = "一点黛眉刀",
     val icon: String = "",
-    var value: Int = 100,
     val part: Int = EquipmentHelper.PART_ALL,  // 部位
-    val id: String = "111111",
     val quality: Int = 0,  // 品质
-    val valueUpgrade: Int = 100,  // 增长属性
     val property: Int = PropertyHelper.PRO_ATTACK,  // 属性
     val basicDesc: String = "",
     val extraDesc: CharSequence = "",
-    val strengthenMax: Int = 100,
+    var value: Int = 100,
+    val valueGrowth: Int = 100,  // 增长属性
     var strengthen: Int = 0,
+    val strengthenGrowth: Int = 100,
+    val forging: Int = 0,
+    val forgingGrowth: Double = 0.0,
     val strengthenAdditions: MutableList<StrengthAdditionVo>? = mutableListOf()
 ) : Parcelable {
 
-    fun enableStrength(): Boolean = strengthen < strengthenMax
+    fun enableStrength(): Boolean = strengthen < 100
 
     fun isQualityMax() = quality >= EquipmentHelper.QUALITY_PEERLESS
 
     constructor(parcel: Parcel) : this(
         parcel.readString() ?: "",
         parcel.readString() ?: "",
-        parcel.readInt(),
-        parcel.readInt(),
         parcel.readString() ?: "",
         parcel.readInt(),
         parcel.readInt(),
@@ -38,24 +38,30 @@ data class EquipmentVo(
         parcel.readString() ?: "",
         parcel.readInt(),
         parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readDouble(),
         arrayListOf<StrengthAdditionVo>().apply {
             parcel.readArrayList(StrengthAdditionVo::class.java.classLoader)
         }
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
         parcel.writeString(name)
         parcel.writeString(icon)
-        parcel.writeInt(value)
         parcel.writeInt(part)
-        parcel.writeString(id)
         parcel.writeInt(quality)
-        parcel.writeInt(valueUpgrade)
         parcel.writeInt(property)
+        parcel.writeInt(value)
+        parcel.writeInt(valueGrowth)
         parcel.writeString(basicDesc)
         parcel.writeString(extraDesc.toString())
-        parcel.writeInt(strengthenMax)
         parcel.writeInt(strengthen)
+        parcel.writeInt(strengthenGrowth)
+        parcel.writeInt(forging)
+        parcel.writeDouble(forgingGrowth)
         parcel.writeList(strengthenAdditions)
     }
 
